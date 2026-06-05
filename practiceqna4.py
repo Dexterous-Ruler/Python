5346102c7d3f1ea4d8c72a2504807d2be843c7e7
Tanishq Aryan <dexterous471@gmail.com>
2026-06-05 23:20:12 +0530

    Add files via upload

---
+++ practiceqna4.py
@@ -0,0 +1,11 @@
+#Write a function greet() that prints "Hello, Python Learner!" when called.
+def greet():
+    print("Hello, Python Learner!")
+greet()  # Call the function to see the output
+
+#Write a function square(num) that returns the square of a given number. Test it with different numbers.
+def square(num):
+    return num * num
+print(square(4))  # Output: 16
+print(square(5))  # Output: 25  
+print(square(10)) # Output: 100
